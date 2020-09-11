4403e5b59ef01a476aa947120221b032854c8e7d
mario8a <mario8ato@hotmail.com>
2020-09-11 14:46:45 -0500

    useReducer

---
+++ src/components/08-useReducer/intro-reducer.js
@@ -0,0 +1,8 @@
+//Establecer estado inicial
+const initialState = [{
+    id: '1',
+    todo: 'Comprar pan',
+    done: false
+}];
+
+//creando el reducer
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { CallBackHook } from './components/06-memos/CallBackHook';
-import { Padre } from './components/07-tarea-memo/Padre';
-// import { MemoHook } from './components/06-memos/MemoHook';
-// import { Memorize } from './components/06-memos/Memorize';
-// import { Layout } from './components/05-useLayoutEffect/layout';
-// import { RealExampleRef } from './components/04-useReff/RealExampleRef';
-// import { FocusScreen } from './components/04-useReff/FocusScreen';
-// import { MultipleCustomHooks } from './components/03-Examples/MultipleCustomHooks';
-// import { FormWithCustomHook } from './components/02-useEffect/formWithCustomHook';
-// import { SimpleForm } from './components/02-useEffect/SimpleForm';
-// import { CounterWidthCustomHook } from './components/01-useState/CounterWidthCustomHook';
-// import { HookApp } from './HookApp';
-// import { CounterApp } from './components/01-useState/CounterApp';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { CallBackHook } from './components/06-memos/CallBackHook';
+// import { Padre } from './components/07-tarea-memo/Padre';
+// // import { MemoHook } from './components/06-memos/MemoHook';
+// // import { Memorize } from './components/06-memos/Memorize';
+// // import { Layout } from './components/05-useLayoutEffect/layout';
+// // import { RealExampleRef } from './components/04-useReff/RealExampleRef';
+// // import { FocusScreen } from './components/04-useReff/FocusScreen';
+// // import { MultipleCustomHooks } from './components/03-Examples/MultipleCustomHooks';
+// // import { FormWithCustomHook } from './components/02-useEffect/formWithCustomHook';
+// // import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// // import { CounterWidthCustomHook } from './components/01-useState/CounterWidthCustomHook';
+// // import { HookApp } from './HookApp';
+// // import { CounterApp } from './components/01-useState/CounterApp';
 
-ReactDOM.render(
-    <Padre/>, 
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//     <Padre/>, 
+//   document.getElementById('root')
+// );
+
+import './components/08-useReducer/intro-reducer'
